88a1ea055443afbee0ff9849cb7a04fc12f8332d
BDA824 <ferneyavila73@gmail.com>
2024-10-05 11:54:14 -0500

    Interface from main menu finished

--- src/Screens/Menu.js
+++ src/Screens/Menu.js
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, View } from "react-native";
-import { IconButton } from "react-native-paper";
+import { Button, IconButton } from "react-native-paper";
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 export default function Menu({ navigation }) {
@@ -10,8 +10,8 @@ export default function Menu({ navigation }) {
                     style={{ fontFamily: 'Montserrat-Bold', fontSize: 14, color: '#ffffff' }} >
                     Esteban-quito
                 </Text>
-                <Text 
-                style={{ color: '#ffffff', fontFamily: 'Montserrat-Light' }} >
+                <Text
+                    style={{ color: '#ffffff', fontFamily: 'Montserrat-Light' }} >
                     Welcome, {'\n'} Andres
                 </Text>
                 <IconButton
@@ -20,9 +20,35 @@ export default function Menu({ navigation }) {
                     onPress={() => navigation.navigate('Login')}
                 />
             </View>
-            <View style={styles.intermediate}></View>
-            <View style={styles.information}></View>
-            <View style={styles.buttons}></View>
+            <View style={{
+                height: 55, 
+                borderTopWidth: 2,
+                borderTopColor: 'black',
+                marginLeft: 12,
+                marginRight: 12
+            }}></View>
+            <View style={styles.information}>
+                <View style={{ paddingTop: 15 }}>
+                    <Text style={{ fontFamily: 'Montserrat-Regular' }}>Saving Account {'\n'} +57 3028561243</Text>
+                </View>
+                <View style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end', paddingRight: 35 }}>
+                    <Text style={{ fontFamily: 'Montserrat-Bold' }}>Balance {'\n'} $ 758,256</Text>
+                </View>
+            </View>
+            <View style={styles.buttons}>
+                <Button
+                    icon={require('../../assets/Icons/ApplyLoan.svg')}
+                    contentStyle={{ flexDirection: 'row-reverse' }}
+                    buttonColor='#271B66'
+                    mode='contained'
+                >Apply for loan</Button>
+                <Button
+                    icon={require('../../assets/Icons/Transaction.svg')}
+                    contentStyle={{ flexDirection: 'row-reverse' }}
+                    buttonColor='#271B66'
+                    mode='contained'
+                >Transactions</Button>
+            </View>
         </View>
     )
 }
@@ -35,7 +61,7 @@ const styles = StyleSheet.create({
     header: {
         height: 125,
         backgroundColor: '#271B66',
-        marginBottom: 85,
+        marginBottom: 65,
         display: 'flex',
         alignItems: 'center',
         flexDirection: 'row',
@@ -43,23 +69,25 @@ const styles = StyleSheet.create({
         borderBottomLeftRadius: 10,
         borderBottomRightRadius: 10
     },
-    intermediate: {
-        height: 75,
-        borderWidth: 2,
-        borderColor: 'gray',
-        borderRadius: 10,
-    },
     information: {
         height: 165,
         borderWidth: 2,
-        borderColor: 'gray',
-        borderRadius: 10,
+        borderColor: '#C4C3C7',
+        borderRadius: 25,
+        display: 'flex',
+        justifyContent: 'space-evenly',
+        paddingLeft: 25,
+        marginLeft: 25,
+        marginRight: 25
     },
     buttons: {
-        height: 100,
-        borderWidth: 2,
-        borderColor: 'gray',
-        borderRadius: 10,
+        height: 200,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginLeft: 25,
+        marginRight: 25,
+        alignItems: 'center',
+        fontFamily: 'Montserrat-Regular'
     }
 })
 
